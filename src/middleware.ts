fa645ab88bcd35351dfdf6895fa39fb4d8766a78
Garrett Tolbert <garrett1.tolbert@gmail.com>
2023-02-14 14:07:56 -0600

    reverted test w/Alice

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,17 +2,9 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-	const response = NextResponse.next();
-	response.headers.set('x-geo', request.geo?.country ?? '');
-
-	// return NextResponse.rewrite(new URL('/api/og', request.url), {
-	// 	headers: response.headers,
-	// });
-	return NextResponse.next({
-		headers: response.headers,
-	});
+	return NextResponse.rewrite(new URL('/api/og', request.url));
 }
 
 export const config = {
-	matcher: ['/', '/config'],
+	matcher: '/',
 };
