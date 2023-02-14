81e88989a36eb9948561405278cd0e25076e585c
Garrett Tolbert <garrett1.tolbert@gmail.com>
2023-02-14 16:45:07 -0600

    updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,3 @@
-import { Suspense, use } from 'react';
-
-interface Props {}
-
-const Skeleton = () => {
-    return <></>;
-}
-
-const ComponentUI = ({}: Props) => {
-    return <></>;
-}
-
-export default function Component(props: Props) {
-    return (
-        <Suspense fallback={<Skeleton />}>
-            <ComponentUI {...props} />
-        </Suspense>
-    );
+export default function Home() {
+	return <div>Home</div>;
 }
