9cd28afca172aed3b85e06c2b5ed13647fa99d01
Garrett Tolbert <garrett1.tolbert@gmail.com>
2023-02-21 11:26:19 -0600

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
-import './globals.css'
+import './globals.css';
+import { AnalyticsWrapper } from './analytics';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      {/*
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang='en'>
+			{/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+			<head />
+			<body>
+				{children}
+				<AnalyticsWrapper />
+			</body>
+		</html>
+	);
 }
